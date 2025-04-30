842acd597ab96db639a56854f41620e39efa6a0e
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-04-30 17:56:11 +0200

    Changed db.js pool function to match railway's

--- src/db.js
+++ src/db.js
@@ -2,14 +2,11 @@ import mysql from 'mysql2/promise'; // Import mysql2/promise for promise-based M
 // import dotenv from 'dotenv'; // Import dotenv to load environment variables
 
 const pool = mysql.createPool({
-    host: process.env.RAILWAY_PRIVATE_DOMAIN,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.MYSQLPORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    host: process.env.MARIADB_HOST,
+  user: process.env.MARIADB_USER || process.env.MARIADB_USERNAME,
+  password: process.env.MARIADB_PASSWORD,
+  database: process.env.MARIADB_DATABASE,
+  port: process.env.MARIADB_PORT || 3306,
 });
 
 export default pool; // Export the connection pool for use in other modules
