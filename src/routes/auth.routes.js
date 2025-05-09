e0b4edf1dd865a870fc62ea07617271d020e1133
K1NG-Devops <king@youngeagles.org.za>
2025-05-09 14:26:08 +0200

    fixed password authentication for login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -78,6 +78,7 @@ router.post('/login', async (req, res) => {
     console.error('Error during login:', err);
     res.status(500).json({ message: 'Server error.' });
   }
+});
 
 // ✅ GET /auth/users (for admin/testing)
 router.get('/users', verifyToken, async (req, res) => {
