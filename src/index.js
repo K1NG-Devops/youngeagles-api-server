e0a1831dbdc289d92943524a7d67143e309809f2
K1NG-Devops <king@youngeagles.org.za>
2025-05-18 22:46:06 +0200

    Added limiter to config

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ connect();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
     message: 'Too many requests from this IP, please try again later.',
   },
 });
+
+app.use(limiter);
 
 
 const app = express();
