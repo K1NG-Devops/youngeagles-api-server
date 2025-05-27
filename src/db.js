5d6c5a50f90b159f7891fb285a8c28577dcbee90
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-05-27 21:50:30 +0200

    Changed destructuring logic

--- src/db.js
+++ src/db.js
@@ -74,7 +74,7 @@ export const query = async (sql, params = [], db = 'skydek_DB') => {
   const pool = getPool(db);
   try {
     const [rows] = await pool.query(sql, params);
-    return result;
+    return rows;
   } catch (error) {
     console.error(`❌ Error executing query on ${db} database:`, error);
     throw error;
